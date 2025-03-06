85a2de41aaff7d5726c918a56ad27866093c2b20
amang <aman.ghazanfar@edu.esiee.fr>
2025-03-06 16:59:58 +0100

    feat: add collision AABB and increase of ball speed on each collision and labels

--- main.js
+++ main.js
@@ -33,7 +33,8 @@ css2DRenderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(css2DRenderer.domElement);
 
 // Balle
-const sphereGeometry = new THREE.SphereGeometry(1);
+const ballRadius = 0.25;
+const sphereGeometry = new THREE.SphereGeometry(ballRadius);
 const sphereMaterial = new THREE.MeshBasicMaterial({color: 0xff0000})
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
@@ -41,7 +42,7 @@ scene.add(sphere);
 
 // Rectangle Left
 
-const rectangleLeftGeometry = new THREE.PlaneGeometry(0.5,2);
+const rectangleLeftGeometry = new THREE.PlaneGeometry(0.25,2);
 const rectangleLeftMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, side: THREE.DoubleSide});
 const rectangleLeft = new THREE.Mesh(rectangleLeftGeometry, rectangleLeftMaterial);
 rectangleLeft.position.x = -5;
@@ -51,7 +52,7 @@ scene.add(rectangleLeft);
 
 // Rectangle right
 
-const rectangleRightGeometry = new THREE.PlaneGeometry(0.5,2);
+const rectangleRightGeometry = new THREE.PlaneGeometry(0.25,2);
 const rectangleRightMaterial = new THREE.MeshBasicMaterial({color: 0x00ff00, side: THREE.DoubleSide});
 const rectangleRight = new THREE.Mesh(rectangleRightGeometry, rectangleRightMaterial);
 rectangleRight.position.x = 5;
@@ -62,42 +63,100 @@ scene.add(rectangleRight);
 /**
  * Animate method, animates the threejs components
  */
+
+let rightCollided = false;
 function animate() {
+   
+    checkSphereBoxesCollision();
+    updateObjectPosition()
     renderer.render(scene,camera);
     css2DRenderer.render(scene,camera);
+}
+let ballSpeed = 0.02;
+let ballAdd = 0.01
+function checkSphereBoxesCollision() {
+    
+
+
+    rectangleRight.updateMatrixWorld(true);
+    rectangleLeft.updateMatrixWorld(true);
+    const rectangleRightBB = new THREE.Box3().setFromObject(rectangleRight);
+    const rectangleLeftBB = new THREE.Box3().setFromObject(rectangleLeft);
+    const sphereBB = new THREE.Box3().setFromObject(sphere);
+    if(sphereBB.intersectsBox(rectangleRightBB) && !rightCollided) {
+        rightCollided = true;
+        ballSpeed += ballAdd;
+    } 
+    
+    
+    if(sphereBB.intersectsBox(rectangleLeftBB)) {
+       rightCollided = false;
+       ballSpeed += ballAdd;
+    } 
+   
+    if(rightCollided)  {
+        sphere.position.x -= ballSpeed;
+    } else {
+        sphere.position.x += ballSpeed;
+    }
 }
 
 renderer.setAnimationLoop(animate)
 
+// Players label
 
-// Ball label
+const playerOneSpan = document.createElement("span");
 
-const div = document.createElement("div");
-div.className = "label";
-div.style.color = "yellow";
-div.textContent = "Balle";
-const label = new CSS2DObject(div);
-label.position.set(0, 6, 0);
-  
-sphere.add(label);
+playerOneSpan.style.color = "yellow";
+playerOneSpan.textContent = "Player 1";
+const playerOneLabel = new CSS2DObject(playerOneSpan);
+rectangleLeft.geometry.computeBoundingBox()
+const rectangleLeftHeight = rectangleLeft.geometry.parameters.height * 7.5;
+playerOneLabel.position.set(0, rectangleLeftHeight / 2, 0);
+playerOneLabel.rotation.set(0, Math.PI, 0)
+rectangleLeft.add(playerOneLabel);
+
+const playerTwoSpan = document.createElement("span");
+
+playerTwoSpan.style.color = "yellow";
+playerTwoSpan.textContent = "Player 2";
+const playerTwoLabel = new CSS2DObject(playerTwoSpan);
+const rectangleRightHeight = rectangleRight.geometry.parameters.height * 7.5;
+playerTwoLabel.position.set(0, rectangleRightHeight / 2, 0);
+
+rectangleRight.add(playerTwoLabel);
 
 // Key events
+
+const keyState = {
+    a: false,
+    q: false,
+    z: false,
+    s: false
+}
+
 window.addEventListener('keydown', (e) => {
     let key = e.key;
-      
-        if(key.toLowerCase() === "a")
-            rectangleLeft.position.y += 0.1;
-           
-        if(key.toLowerCase() === "q")
-            rectangleLeft.position.y -= 0.1;
-        if(key.toLowerCase() === "z")
-            rectangleRight.position.y += 0.1;
-           
-        if(key.toLowerCase() === "s")
-            rectangleRight.position.y -= 0.1;
-
-    
+    if(keyState.hasOwnProperty(key)) {
+        keyState[key] = true;
+    }
+       
 });
+
+window.addEventListener('keyup', (e) => {
+    let key = e.key;
+    if(keyState.hasOwnProperty(key)) {
+        keyState[key] = false;
+    }
+       
+});
+
+function updateObjectPosition() {
+    if(keyState['a']) rectangleLeft.position.y += 0.1;
+    if(keyState['q']) rectangleLeft.position.y -= 0.1;
+    if(keyState['z']) rectangleRight.position.y += 0.1;
+    if(keyState['s']) rectangleRight.position.y -= 0.1;
+}
 
 // Resize event
 
